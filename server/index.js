1a8093b15828dd73cf19796e1bd864a616563c77
a-ayushigit <116533619+a-ayushigit@users.noreply.github.com>
2024-04-18 07:45:40 +0530

    New Commit
    Connected to database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
+const cors = require('cors');
+const  connectdb = require( './db/connect');
+require('dotenv').config();
+
+//Middleware 
+app.use(cors());
+app.use(express.json());
+//helps in making connection to the frontend side 
 
 app.get('/',(req,res)=>{
     res.send("Hello World !");
 })
 
-app.listen(port , ()=>{
-    console.log(`App listening on port ${port}`);
-})
+const start = async() =>{
+    try {
+    
+    await connectdb(process.env.MONGO_URI);
+     app.listen(port , console.log(`Server is listening to port  ${port}..`));
+    }
+    catch(error){
+      console.log(error);
+    }
+ }
+ 
+ start();
